160cf0f85e8524c8e89803ebf6fdc012beafefcf
Irina Belova <i.belova@infinet.ru>
2021-05-14 15:43:32 +0500

    tutorial 3

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # For more information, please refer to https://aka.ms/vscode-docker-python
-FROM irrr/py310
+FROM irrr/python-django:latest
 
 # Keeps Python from generating .pyc files in the container
 ENV PYTHONDONTWRITEBYTECODE=1
--- api/api/settings.py
+++ api/api/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&o_$n*8$*jp4hw=w8fb_7*%wmvv0cb8w$drc!plkbdjx*-u$d('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
