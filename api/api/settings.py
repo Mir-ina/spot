a2a66c13f400be8af89b3afce018da0bb0d57be6
Irina Belova <i.belova@infinet.ru>
2021-05-17 15:00:30 +0500

    tutorial 4

--- api/api/settings.py
+++ api/api/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&o_$n*8$*jp4hw=w8fb_7*%wmvv0cb8w$drc!plkbdjx*-u$d('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
